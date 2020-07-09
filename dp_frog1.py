eb935a5d5826efd8f12ae0155c369a57f3a7b19a
geoffc <geoffc@qualtrics.com>
2020-07-08 21:14:59 -0700

    refactored frog_jump dynamic programming exercise

--- dp_frog1.py
+++ dp_frog1.py
@@ -2,25 +2,19 @@ import numpy as np
 from pprint import pprint
 
 def min_cost(N: int, h: list) -> int:
-    memo = np.empty((N, N))
+    if N < 2:
+        return 0
 
-    #We will use zero indexing and assume the first 
-    #stone starts at index 0
-    #the row, i, represents the starting point
-    #the column, j, represents the ending point
-    for i in range(0, N):
-        for j in range(i, i+3):
-            if j < N:
-                if i == 0:
-                    min_to_date = 0
-                elif i == 1:
-                    min_to_date = memo[i-1,i]
-                else:
-                    min_to_date = min(memo[i-2,i], memo[i-1,i])
-                jump_cost = abs(h[i] - h[j])
-                memo[i, j] = min_to_date + jump_cost
+    #each element in the list represents the min cost of getting from the beginning to that element
+    cost = [None] * N
+    cost[0] = 0
+    cost[1] = abs(h[0] - h[1])
+    for i in range(2, N):
+        single_jump = abs(h[i] - h[i-1]) + cost[i-1]
+        double_jump = abs(h[i] - h[i-2]) + cost[i-2]
+        cost[i] = min(single_jump, double_jump)
 
-    return memo[N-1,N-1]
+    return cost[N-1]
 
 assert min_cost(N=4, h=[10,30,40,20]) == 30
 assert min_cost(N=2, h=[10,10]) == 0
